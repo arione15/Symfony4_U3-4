542d6cb93353d4faf7e4ac5891484dfdc3233d46
Massine <massinoueslati@gmail.com156>
2021-07-14 19:21:21 +0200

    Fixtures Aliments + page de listing de tuos les aliments : utilisation du thème Bootswatch "litera" + ajout d'un fichier main.css pour la taille des images

---
+++ .history/src/Controller/AlimentController_20210714181806.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AlimentController extends AbstractController
+{
+    /**
+     * @Route("/", name="aliments")
+     */
+    public function index(): Response
+    {
+        return $this->render('aliment/aliments.html.twig');
+    }
+}
---
+++ .history/src/Controller/AlimentController_20210714185213.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AlimentController extends AbstractController
+{
+    /**
+     * @Route("/", name="aliments")
+     */
+    public function index(): Response
+    {
+        return $this->render('aliment/aliments.html.twig');
+    }
+
+    /**
+    * @Route("/aliments", name="afficher_aliments")
+     */
+    public function afficherAliments(): Response
+    {
+        $aliments = $repository -> findAll()
+        return $this->render('aliment/afficher_aliments.html.twig');
+    }
+}
